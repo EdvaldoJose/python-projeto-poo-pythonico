97851127618adfb4f3d14628e61a20789d3dffe5
Edvaldo José <edvaldojose003@gmail.com>
2023-07-10 15:47:42 -0300

    feat: classe Produto

--- src/produto.py
+++ src/produto.py
@@ -1,20 +1,43 @@
 class Produto:
     def __init__(
-        self, nome: str, codigo: str, preco: float, quantidade: int
+        self,
+        nome: str,
+        codigo: str,
+        preco: float,
+        quantidade: int
     ) -> None:
-        pass
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__preco = preco
+        self.__quantidade = quantidade
 
     def get_preco(self) -> float:
-        pass
+        return self.__preco
 
     def get_quantidade(self) -> int:
-        pass
+        return self.__quantidade
 
-    def atualizar_preco_do_produto(self, novo_preco: float) -> None:
-        pass
+    def atualizar_preco_do_produto(
+        self,
+        novo_preco: float
+    ) -> None:
+        if novo_preco > 0:
+            raise ValueError("O preco nao pode ser negativo")
+        self.__preco = novo_preco
 
-    def adicionar_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+    def adicionar_estoque_do_produto(
+        self,
+        quantidade: int
+    ) -> None:
+        self.__quantidade += quantidade
 
-    def remover_estoque_do_produto(self, quantidade: int) -> None:
-        pass
+    def remover_estoque_do_produto(
+        self,
+        quantidade: int
+    ) -> None:
+        if self.__quantidade > quantidade:
+            raise ValueError("""
+            Quantidade informada e maior que a quantidade,
+            disponivel em estoque.
+             """)
+        self.__quantidade -= quantidade
