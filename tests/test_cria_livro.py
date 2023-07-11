d481e88292995343c6cf9270e3ece2e301f9575e
Edvaldo José <edvaldojose003@gmail.com>
2023-07-10 21:32:15 -0300

    feat: correcao class livro

--- tests/test_cria_livro.py
+++ tests/test_cria_livro.py
@@ -2,7 +2,11 @@ from src.livro.livro import Livro
 
 
 def test_cria_livro():
-    livro = Livro("Pense em Python", "Allen B. Downey", 309)
-    assert livro.titulo == "Pense em Python"
-    assert livro.autor == "Allen B. Downey"
+    titulo = "Pense em Python"
+    autor = "Allen B. Downey"
+    paginas = 309
+
+    livro = Livro(titulo, autor, paginas)
+    assert livro.titulo == titulo
+    assert livro.autor == autor
     assert livro.paginas == 309
